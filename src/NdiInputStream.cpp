67e824561df0967f74de2852f26afeda5d210887
Naoto Kondo <cgigcp3yqt@gmail.com>
2024-10-20 08:07:53 +0900

    get ndi source from manager

--- src/NdiInputStream.cpp
+++ src/NdiInputStream.cpp
@@ -1,4 +1,5 @@
 #include "NdiInputStream.hpp"
+#include "NdiManager.hpp"
 
 #include <godot_cpp/classes/os.hpp>
 #include <godot_cpp/core/class_db.hpp>
@@ -74,67 +75,76 @@ void NdiInputStream::set_bandwidth(Bandwidth bandwidth) {
 }
 
 void NdiInputStream::_thread_function() {
+
   while (!_should_exit) {
-    NDIlib_source_t source;
-    source.p_ndi_name = _source_name.utf8().get_data();
 
-    NDIlib_recv_create_v3_t recv_create_desc;
-    recv_create_desc.source_to_connect_to = source;
-    recv_create_desc.color_format = NDIlib_recv_color_format_BGRX_BGRA;
-    recv_create_desc.bandwidth = (_bandwidth == HIGHEST)
-                                     ? NDIlib_recv_bandwidth_highest
-                                     : NDIlib_recv_bandwidth_lowest;
+    uint64_t ptr = NdiManager::get_singleton()->get_source(_source_name);
 
-    auto _ndi_receiver = NDIlib_recv_create_v3(&recv_create_desc);
+    if (auto p_source = reinterpret_cast<NDIlib_source_t *>(ptr)) {
 
-    if (_ndi_receiver) {
-      NDIlib_metadata_frame_t meta_frame;
-      const char *meta = "<ndi_hwaccel enabled=\"true\"/>";
-      meta_frame.p_data = const_cast<char *>(meta);
-      NDIlib_recv_send_metadata(_ndi_receiver, &meta_frame);
+      NDIlib_recv_create_v3_t pNDI_recv_create;
 
-      bool continue_receiving = true;
-      while (!_should_exit && continue_receiving) {
-        NDIlib_video_frame_v2_t video_frame;
-        NDIlib_audio_frame_v3_t audio_frame;
-
-        switch (NDIlib_recv_capture_v3(_ndi_receiver, &video_frame,
-                                       &audio_frame, nullptr, 5000)) {
-        case NDIlib_frame_type_video: {
-          PackedByteArray image_data;
-          image_data.resize(video_frame.xres * video_frame.yres * 4);
-          memcpy(image_data.ptrw(), video_frame.p_data, image_data.size());
-
-          Ref<Image> new_frame =
-              Image::create_from_data(video_frame.xres, video_frame.yres, false,
-                                      Image::FORMAT_RGBA8, image_data);
-
-          _mutex->lock();
-          _frame = new_frame;
-          _frame_count++;
-          _mutex->unlock();
-
-          NDIlib_recv_free_video_v2(_ndi_receiver, &video_frame);
-          break;
-        }
-        case NDIlib_frame_type_audio:
-          NDIlib_recv_free_audio_v3(_ndi_receiver, &audio_frame);
-          break;
-        case NDIlib_frame_type_none:
-          continue_receiving = false;
-          break;
-        }
-
-        OS::get_singleton()->delay_usec(5000); // 5ms delay
+      if (_bandwidth == HIGHEST) {
+        pNDI_recv_create.bandwidth = NDIlib_recv_bandwidth_highest;
+      } else if (_bandwidth == LOWEST) {
+        pNDI_recv_create.bandwidth = NDIlib_recv_bandwidth_lowest;
       }
 
-      NDIlib_recv_destroy(_ndi_receiver);
-      _ndi_receiver = nullptr;
+      pNDI_recv_create.color_format = NDIlib_recv_color_format_e_RGBX_RGBA;
 
-      _mutex->lock();
-      _frame = Ref<Image>();
-      _frame_count++;
-      _mutex->unlock();
+      if (auto pNDI_recv = NDIlib_recv_create_v3(&pNDI_recv_create)) {
+
+        NDIlib_recv_connect(pNDI_recv, p_source);
+
+        NDIlib_metadata_frame_t meta_frame;
+        const char *meta = "<ndi_hwaccel enabled=\"true\"/>";
+        meta_frame.p_data = const_cast<char *>(meta);
+        NDIlib_recv_send_metadata(pNDI_recv, &meta_frame);
+
+        bool continue_receiving = true;
+        while (!_should_exit && continue_receiving) {
+
+          NDIlib_video_frame_v2_t video_frame;
+          NDIlib_audio_frame_v3_t audio_frame;
+
+          switch (NDIlib_recv_capture_v3(pNDI_recv, &video_frame, &audio_frame,
+                                         nullptr, 5000)) {
+          case NDIlib_frame_type_video: {
+            PackedByteArray image_data;
+            image_data.resize(video_frame.xres * video_frame.yres * 4);
+            memcpy(image_data.ptrw(), video_frame.p_data, image_data.size());
+
+            Ref<Image> new_frame =
+                Image::create_from_data(video_frame.xres, video_frame.yres,
+                                        false, Image::FORMAT_RGBA8, image_data);
+
+            _mutex->lock();
+            _frame = new_frame;
+            _frame_count++;
+            _mutex->unlock();
+
+            NDIlib_recv_free_video_v2(pNDI_recv, &video_frame);
+            break;
+          }
+          case NDIlib_frame_type_audio:
+            NDIlib_recv_free_audio_v3(pNDI_recv, &audio_frame);
+            break;
+          case NDIlib_frame_type_none:
+            continue_receiving = false;
+            break;
+          }
+
+          OS::get_singleton()->delay_usec(5000); // 5ms delay
+        }
+
+        NDIlib_recv_destroy(pNDI_recv);
+        pNDI_recv = nullptr;
+
+        _mutex->lock();
+        _frame = Ref<Image>();
+        _frame_count++;
+        _mutex->unlock();
+      }
     }
 
     OS::get_singleton()->delay_msec(500);
